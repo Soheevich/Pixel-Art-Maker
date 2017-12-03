0f788de4dd14c4909cb7d5b333c9711f0bc56fdb
Soheevich <soheevich@gmail.com>
2017-12-03 08:43:11 +0300

    Upload to Udacity

--- designs.js
+++ designs.js
@@ -1,41 +1,28 @@
 var color = $("#colorPicker").val();
 const canvas = $("#pixel_canvas");
 
-/*
-Сделать проверку на лету, чтобы показывалось предупреждение, в случае неверного ввода чисел
+//Function for Making grid
+function makeGrid(height, width) {
+  //Building rows and columns
+  for (let row = 0; row < height; row++) {
+    canvas.append("<tr>");
+    let col = 0;
+    while (col < width) {
+      canvas.children().last().append("<td></td>");
+      col += 1;
+    }
+    canvas.append("</tr>");
+  }
+}
 
-Стирать по ПКМ http://api.jquery.com/event.which/
-В CSS затемнять клетки при прохождении курсора
-Максимально задействовать CSS, пока хз как
-Настроить UI и шрифты
-Настроить таблицу цветов
-id в таблице можно использовать, чтобы делать потом картинку
-Генерировать сетку только в один квадрат, после вызвать функцию, которая щелкнет в начале, ее узнать координату в левом верхнем углу, потом по координатам курсора уже рисовать.
-
-*/
 
 // Listening for color change
 $("#colorPicker").on("change", function() {
   color = $("#colorPicker").val();
 });
 
-//Function for Making grid
-function makeGrid(height, width) {
-  let tablePos = canvas;
-  //Building rows and columns, adding ids
-  for (let row = 0; row < height; row++) {
-    tablePos.append("<tr>");
-    let col = 0;
-    while (col < width) {
-      tablePos.children().last().append(`<td id="${row}-${col}"></td>`);
-      col += 1;
-    }
-    tablePos.append("</tr>");
-  }
-}
 
-
-// Disable button if input is incorrect
+// Disable "Make grid" button if input is incorrect
 $("input").on("change", function() {
   let h = Number($("#input_height").val());
   let w = Number($("#input_width").val());
@@ -66,7 +53,7 @@ $(":button").click(function() {
 
 // Drawing and Erasing
 canvas.on("mousedown", "td", function(event) {
-  event.preventDefault();
+  event.preventDefault(); // No drag'n'drop attempts
 
   if (event.which === 1) {
 
@@ -86,13 +73,13 @@ canvas.on("mousedown", "td", function(event) {
       }
       $(this).css("background-color", color);
     });
-  } else if (event.which === 3) {
 
     // Erasing
+  } else if (event.which === 3) {
     let erase = true;
     $(this).css("background-color", "white");
 
-    // Listening for mouseUp
+    // Listening for mouseUp and disabling contextmenu
     $(document).on("mouseup contextmenu", function() {
       event.preventDefault();
       return erase = false;
